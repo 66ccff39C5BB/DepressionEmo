b6fdd8386a0b5f322a3cb9ba61fbee885e72340a
66ccff39C5BB <lip.f@seu.edu.cn>
2024-12-02 17:37:03 +0800

    change xgb to BR method

--- xgb.py
+++ xgb.py
@@ -1,97 +1,69 @@
-import pandas as pd
+import argparse
+import random
 import numpy as np
+import pandas as pd
+import nltk
+from nltk.corpus import stopwords
+import matplotlib.pyplot as plt
+from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+import pickle
+import time
+import re
+import os
+from tqdm import tqdm,trange
 
 from file_io import *
 
+#from transformers import AutoTokenizer
+#tokenizer = AutoTokenizer.from_pretrained('bert-base-cased')
+
 emotion_list = ['anger', 'brain dysfunction (forget)', 'emptiness', 'hopelessness', 'loneliness', 'sadness', 'suicide intent', 'worthlessness']
+symptom_list = ['Sadness', 'Pessimism', 'Sense_of_failure', 'Loss_of_Pleasure', 'Guilty_feelings', 'Sense_of_punishment', 'Self-dislike', 'Self-incrimination', 'Suicidal_ideas', 'Crying', 'Agitation', 'Social_withdrawal', 'Indecision', 'Feelings_of_worthlessness', 'Loss_of_energy', 'Change_of_sleep', 'Irritability', 'Changes_in_appetite', 'Concentration_difficulty', 'Tiredness_or_fatigue', 'Loss_of_interest_in_sex']
 
-def convert_labels(labels):
+
+current_time = time.strftime("%Y%m%d%H%M%S")
+savedir = os.path.join('xgb', current_time)
+os.makedirs(savedir)
+parser = argparse.ArgumentParser(description='Training Parameter')
+parser.add_argument('--train_path', type=str, default='Dataset/train.json') 
+parser.add_argument('--test_path', type=str, default='Dataset/test.json')
+parser.add_argument('--val_path', type=str, default='Dataset/val.json')
+args = parser.parse_args()
+
+label_list = symptom_list if "BDISen" in args.test_path else emotion_list
+
     
-    labels2 = []
-    for idx, label in enumerate(labels):
-        try: 
-            label = str(label[0])
-        except:
-            label = str(label)
-            pass
+train_set = read_list_from_jsonl_file(args.train_path)
+val_set = read_list_from_jsonl_file(args.val_path)
+test_set = read_list_from_jsonl_file(args.test_path)
 
-        temp = []
-        num = len(emotion_list) - len(label)
-        if (num == 0): 
-            temp = [int(x) for x in label]
-        else:
-            temp = ''.join(['0']*num) + str(label)   
-            temp = [int(x) for x in temp]
-            
-        labels2.append(temp)
-        
-    return labels2
-    
-    
-train_set = read_list_from_jsonl_file('dataset/train.json')
-val_set = read_list_from_jsonl_file('dataset/val.json')
-test_set = read_list_from_jsonl_file('dataset/test.json')
-train_set = train_set  + test_set + val_set
+dataset = train_set + val_set + test_set
 
-data_labels = [item['label_id'] for item in train_set]
-df_labels = pd.DataFrame(data_labels, columns=['labels'])
-print('df_labels: ', df_labels)
+data_labels = [[] for i in range(len(label_list))]
+for item in dataset:
+    data_label = str(item['label_id'])
+    while len(data_label) < len(label_list):
+        data_label = '0' + data_label
+    for i in range(len(data_label)):
+        data_labels[i].append(float(data_label[i]))
 
-import nltk
-from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
-import re
+df_labels = np.concatenate([np.array(data_labels[i])[np.newaxis, :] for i in range(len(label_list))], axis=0)
 
-# Download necessary NLTK corpora
-nltk.download('stopwords')
-nltk.download('wordnet')
-
-stop_words = set(stopwords.words('english'))
-lemma = WordNetLemmatizer()
-
-def clean_text(text):
-    
-
-    '''text = re.sub(r'http\S+', '', text)
-    text = re.sub(r'[^\w\s]', '', text, re.UNICODE) # remove punctuation
-    
-    # cleaning everything except alphabetical and numerical characters
-    text = re.sub("[^a-zA-Z0-9]"," ",text)
-    
-    # tokenizing and lemmatizing
-    text = nltk.word_tokenize(text.lower())
-    text = [lemma.lemmatize(word) for word in text]
-    
-    # removing stopwords
-    text = [word for word in text if word not in stop_words]
-    
-    # joining
-    text = " ".join(text)'''
-    
-    return text
-
-for item in train_set: item['text'] = clean_text(item['text'])
-
-for i in range(0,5): print(train_set[i]['text']
-
-,end="\n\n")
-    
-from sklearn.feature_extraction.text import TfidfVectorizer
+data = []
+for item in dataset:
+    text = item['text']        
+    data.append(text)
 
 vectorizer = TfidfVectorizer()
-#vectorizer = TfidfVectorizer(max_features=5000)
-data_features = vectorizer.fit_transform([item['text'] for item in train_set])
-data_features = data_features.toarray()
+BOW = vectorizer.fit_transform(data)
 
-from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(data_features, np.asarray(df_labels), test_size=0.3, train_size = 0.7, random_state=0)
-x_test, x_val, y_test, y_val = train_test_split(x_test, y_test, test_size=0.5, random_state=0)
+x_train, y_train = BOW[:len(train_set)], df_labels[:, :len(train_set)]
+x_val, y_val = BOW[len(train_set):len(train_set)+len(val_set)], df_labels[:, len(train_set):len(train_set)+len(val_set)]
+x_test, y_test = BOW[len(train_set)+len(val_set):], df_labels[:, len(train_set)+len(val_set):]
 
 print(x_train.shape)
 print(y_train.shape)
-
-print(x_val.shape)
-print(y_val.shape)
 
 print(x_test.shape)
 print(y_test.shape)
@@ -99,21 +71,15 @@ print(y_test.shape)
 import xgboost as xgb
 
 # learning_rate = 0.01
-model = xgb.XGBClassifier(max_depth=8, n_estimators=100, learning_rate=0.5)
+models = [xgb.XGBClassifier(max_depth=8, n_estimators=100, learning_rate=0.5) for i in range(len(label_list))]
+for i in trange(len(label_list), desc='Training'):
+    models[i].fit(x_train,y_train[i])
+    pickle.dump(models[i], open(savedir+f"/model_{i}.pkl", "wb"))
 
-from sklearn.preprocessing import LabelEncoder
-le = LabelEncoder()
-y_train = le.fit_transform(y_train.ravel())
-
-model.fit(x_train, y_train)
-
-predictions = model.predict(x_test)
-predictions = le.inverse_transform(predictions)
-predictions = convert_labels(predictions)
-print('predictions: ', predictions)
-
-y_test = convert_labels(y_test)
-print('y_test: ', y_test)
+predictions = []
+for i in range(len(label_list)):
+    predictions.append(np.array(models[i].predict(x_test))[np.newaxis, :])
+predictions = np.concatenate(predictions, axis=0)
 
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, confusion_matrix
 
