a4c86946a8ab5c3de42814f53d99b4d27feaed1e
66ccff39C5BB <lip.f@seu.edu.cn>
2024-12-02 16:27:35 +0800

    change svm to BR method

--- svm.py
+++ svm.py
@@ -1,3 +1,5 @@
+import argparse
+import random
 import numpy as np
 import pandas as pd
 import nltk
@@ -15,87 +17,49 @@ from file_io import *
 #tokenizer = AutoTokenizer.from_pretrained('bert-base-cased')
 
 emotion_list = ['anger', 'brain dysfunction (forget)', 'emptiness', 'hopelessness', 'loneliness', 'sadness', 'suicide intent', 'worthlessness']
+symptom_list = ['Sadness', 'Pessimism', 'Sense_of_failure', 'Loss_of_Pleasure', 'Guilty_feelings', 'Sense_of_punishment', 'Self-dislike', 'Self-incrimination', 'Suicidal_ideas', 'Crying', 'Agitation', 'Social_withdrawal', 'Indecision', 'Feelings_of_worthlessness', 'Loss_of_energy', 'Change_of_sleep', 'Irritability', 'Changes_in_appetite', 'Concentration_difficulty', 'Tiredness_or_fatigue', 'Loss_of_interest_in_sex']
 
-def convert_labels(labels):
-    
-    labels2 = []
-    for idx, label in enumerate(labels):
-        try: 
-            label = str(label[0])
-        except:
-            label = str(label)
-            pass
 
-        temp = []
-        num = len(emotion_list) - len(label)
-        if (num == 0): 
-            temp = [int(x) for x in label]
-        else:
-            temp = ''.join(['0']*num) + str(label)   
-            temp = [int(x) for x in temp]
-            
-        labels2.append(temp)
-        
-    return labels2
-    
-train_set = read_list_from_jsonl_file('dataset/train.json')
-val_set = read_list_from_jsonl_file('dataset/val.json')
-test_set = read_list_from_jsonl_file('dataset/test.json')
+current_time = time.strftime("%Y%m%d%H%M%S")
+parser = argparse.ArgumentParser(description='Training Parameter')
+parser.add_argument('--train_path', type=str, default='Dataset/train.json') 
+parser.add_argument('--test_path', type=str, default='Dataset/test.json')
+parser.add_argument('--val_path', type=str, default='Dataset/val.json')
+args = parser.parse_args()
 
-train_set = train_set  + test_set + val_set
-print(len(train_set))
+label_list = symptom_list if "BDISen" in args.test_path else emotion_list
 
-data_labels = [item['label_id'] for item in train_set]
+    
+train_set = read_list_from_jsonl_file(args.train_path)
+val_set = read_list_from_jsonl_file(args.val_path)
+test_set = read_list_from_jsonl_file(args.test_path)
 
-df_labels = pd.DataFrame(data_labels, columns=['labels'])
-print('df_labels: ', df_labels)
+dataset = train_set + val_set + test_set
 
-cleaned_data = []
+data_labels = [[] for i in range(len(label_list))]
+for item in dataset:
+    data_label = str(item['label_id'])
+    while len(data_label) < len(label_list):
+        data_label = '0' + data_label
+    for i in range(len(data_label)):
+        data_labels[i].append(float(data_label[i]))
 
-lemma = WordNetLemmatizer()
-stop_words = stopwords.words("english")
-for item in train_set:
-    
-    text = item['text']
-    
-    '''text = tokenizer.encode_plus(text, max_length=256, add_special_tokens=True, return_token_type_ids=False, padding = "max_length", return_attention_mask=True, return_tensors='pt')
-    text = ' '.join(str(x) for x in text['input_ids'].tolist()[0])'''
-    
-    '''text = re.sub(r'http\S+', '', text)
-    text = re.sub(r'[^\w\s]', '', text, re.UNICODE) # remove punctuation
-    
-    # cleaning everything except alphabetical and numerical characters
-    text = re.sub("[^a-zA-Z0-9]"," ",text)
-    
-    # tokenizing and lemmatizing
-    text = nltk.word_tokenize(text.lower())
-    text = [lemma.lemmatize(word) for word in text]
-    
-    # removing stopwords
-    text = [word for word in text if word not in stop_words]
-    
-    # joining
-    text = " ".join(text)'''
-    
-    cleaned_data.append(text)
+df_labels = np.concatenate([np.array(data_labels[i])[np.newaxis, :] for i in range(len(label_list))], axis=0)
 
-for i in range(0,5):
-    print(cleaned_data[i],end="\n\n")
+data = []
+for item in dataset:
+    text = item['text']        
+    data.append(text)
 
-# max_features
-#vectorizer = CountVectorizer(max_features=5000)
 vectorizer = TfidfVectorizer()
-BOW = vectorizer.fit_transform(cleaned_data)
+BOW = vectorizer.fit_transform(data)
 
-from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(BOW, np.asarray(df_labels), test_size=0.3, train_size = 0.7, random_state=0)
-x_test, x_val, y_test, y_val = train_test_split(x_test, y_test, test_size=0.5, random_state=0)
+x_train, y_train = BOW[:len(train_set)], df_labels[:, :len(train_set)]
+x_val, y_val = BOW[len(train_set):len(train_set)+len(val_set)], df_labels[:, len(train_set):len(train_set)+len(val_set)]
+x_test, y_test = BOW[len(train_set)+len(val_set):], df_labels[:, len(train_set)+len(val_set):]
 
 print(x_train.shape)
 print(y_train.shape)
-
-print(x_val.shape)
-print(y_val.shape)
 
 print(x_test.shape)
 print(y_test.shape)
@@ -103,22 +67,20 @@ print(y_test.shape)
 from sklearn.svm import SVC
 start_time = time.time()
 
-model = SVC() 
-model.fit(x_train,y_train)
+models = [SVC() for i in range(len(label_list))]
+for i in range(len(label_list)):
+    models[i].fit(x_train,y_train[i])
 
 end_time = time.time()
 process_time = round(end_time-start_time, 2)
 print("Fitting SVC took {} seconds".format(process_time))
 
-predictions = model.predict(x_test)
+predictions = []
+for i in range(len(label_list)):
+    predictions.append(np.array(models[i].predict(x_test))[np.newaxis, :])
+predictions = np.concatenate(predictions, axis=0)
 
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, confusion_matrix
-
-print('y_test: ', y_test)
-print('predictions: ', predictions)
-
-y_test = convert_labels(y_test)
-predictions = convert_labels(predictions)
 
 f1_mi = f1_score(y_true=y_test, y_pred=predictions, average='micro')
 re_mi = recall_score(y_true=y_test, y_pred=predictions, average='micro')
